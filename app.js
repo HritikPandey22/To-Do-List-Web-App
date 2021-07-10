9ee99db1aaa5d84caa11c25b616faf8b8984cd26
Hritik <hritikp2295@gmail.com>
2021-07-10 23:39:38 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+// const item1 = new Item({
+//     name : "Welcome to your todolist!"
+// })
+
+// const item2 = new Item({
+//     name : "Hit the + button to add a new item"
+// })
+// const item3 = new Item({
+//     name : "<-- Hit this to delete an item"
+// })
+
+// const defaultItems = [item1,item2,item3];
+
+
+
+app.get("/",function(req,res){
+    
+    Item.find({}, function(err,foundItems){
+       
+            res.render("list" , {newListItems: foundItems});
+        
+    });
+    
+});
+
+app.post("/delete", function(req,res){
+    const checkedItemId = req.body.checkbox;
+
+    Item.findByIdAndDelete(checkedItemId , function(err){
+        if(!err){
+            console.log("deleted");
+            res.redirect("/");
+        }
+    });
+})
+
+
+app.post("/",function(req,res){
+    const itemName = req.body.newItem;
+
+    const item = new Item({
+        name : itemName
+    });
+
+    item.save();
+
+    res.redirect("/");
+});
+
+
+app.listen(5000,function(){
+    console.log("server started on port 5000");
+})
